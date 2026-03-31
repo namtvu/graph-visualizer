e6f98bf54b5735ea28f7b9f65dd5c755d69ff0db
namtvu <91674062+namtvu@users.noreply.github.com>
2026-03-31 21:31:35 +0700

    Update bfs.js

--- static/bfs.js
+++ static/bfs.js
@@ -1,38 +1,30 @@
 function runBFS(){
     let g = buildGraph();
+    if(nodes.length === 0) return;
 
-    let visited = new Set();
-    let queue = [0];
-
-    showCode(`BFS(G,s)
-1 enqueue s
-2 while queue not empty
-3   v = dequeue
-4   visit v
-5   enqueue neighbors`);
+    let visited=new Set();
+    let queue=[0];
 
     function step(){
-        if(queue.length === 0){
-            updateData([]);
+        if(queue.length===0){
+            updateData("Done");
             return;
         }
 
-        updateData(queue);
+        updateData(JSON.stringify(queue));
 
-        let v = queue.shift();
+        let v=queue.shift();
 
         if(!visited.has(v)){
             visited.add(v);
             highlight(v);
 
             for(let u of g[v]){
-                if(!visited.has(u)){
-                    queue.push(u);
-                }
+                if(!visited.has(u)) queue.push(u);
             }
         }
 
-        setTimeout(step,800);
+        setTimeout(step,700);
     }
 
     step();
